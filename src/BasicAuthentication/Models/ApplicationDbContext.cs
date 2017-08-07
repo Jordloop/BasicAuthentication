f8dd7752f2be44dce7d8d861938afb3581fdc2ba
jordloop <jordloop@gmail.com>
2017-08-07 09:28:19 -0700

    Add project files.

---
+++ src/BasicAuthentication/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//Manages Users
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//DB
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicAuthentication.Models
+{
+    //Identity comes with a class to represent a user 'IdentityDbContext<ApplicationUser'
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ src/BasicAuthentication/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//Used for managing users
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace BasicAuthentication.Models
+{
+    //Identity comes with a class to represent a user 'IdentityUser'
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
